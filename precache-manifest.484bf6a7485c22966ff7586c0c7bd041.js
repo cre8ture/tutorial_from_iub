fd8dc0560ce91b24d8fac054783842ec07f7aed5
cre8ture <kai@thebodyliterate.com>
2021-12-23 08:59:40 -0500

    Updates

---
+++ precache-manifest.484bf6a7485c22966ff7586c0c7bd041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8bab4acc26dc601c35",
+    "url": "/tutorial_from_iub/static/css/main.d6bbd77d.chunk.css"
+  },
+  {
+    "revision": "ca8bab4acc26dc601c35",
+    "url": "/tutorial_from_iub/static/js/main.8ce3b3c1.chunk.js"
+  },
+  {
+    "revision": "f796076df1de88946638",
+    "url": "/tutorial_from_iub/static/js/runtime~main.b77139f5.js"
+  },
+  {
+    "revision": "d060c5f22e9ea2b80a60",
+    "url": "/tutorial_from_iub/static/js/2.e4b50371.chunk.js"
+  },
+  {
+    "revision": "d17007c54a1b69692a0febc207601756",
+    "url": "/tutorial_from_iub/index.html"
+  }
+];
